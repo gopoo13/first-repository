96dedcd6e314026960dfa7fac27ef1cd45f51267
gopoo13 <sungmin33527829@gmail.com>
2024-09-12 17:40:06 +0900

    Third

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,17 @@
 def print_hello():
     print("hello!")
 
-def pr():
-    print("kit")
+def pre():
+    print("commit")
+    print_hello()
+
+a = 3
+b = 2
+c = a + b
 
 if __name__ = '__main__':
     print("hello")
     print_hello()
+    pre()
+
+print(c)
